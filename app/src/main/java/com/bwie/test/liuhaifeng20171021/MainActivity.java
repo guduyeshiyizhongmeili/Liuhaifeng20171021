4c69953a39d7415f9027a1d24690a8c1f441988b
liuhaifeng <1932736402@qq.com>
2017-10-21 11:49:26 +0800

    加入拦截

--- app/src/main/java/com/bwie/test/liuhaifeng20171021/MainActivity.java
+++ app/src/main/java/com/bwie/test/liuhaifeng20171021/MainActivity.java
@@ -3,11 +3,15 @@ package com.bwie.test.liuhaifeng20171021;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import okhttp3.OkHttpClient;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
